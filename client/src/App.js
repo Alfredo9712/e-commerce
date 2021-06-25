8b6c752f12b2d52c45b66f91971e9c98bc4f4749
hector27 <hector27c7@gmail.com>
2021-06-25 15:57:22 -0700

    test of pulling data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Button } from 'react-bootstrap';
 function App() {
+  const [products, setProducts] = useState([]);
+  const fetchApi = async () => {
+    const res = await axios.get('http://localhost:5000/product');
+    setProducts(res.data);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
     <div className='App'>
       <Button className='btn-primary'>button</Button>
+      {products.map((product) => (
+        <img src={product.image} />
+      ))}
     </div>
   );
 }
