8dd58f3dafe8d18d25b55bec7b098215dd264ba9
Alfredo Mora <alfredomora@Alfredos-Mac-mini.local>
2021-07-06 13:31:31 -0700

    working on routes for products

--- backend/routes/allProductsRoutes.js
+++ backend/routes/allProductsRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const PantProduct = require("../schema/pantProductSchema");
 const ShirtProduct = require("../schema/shirtProductSchema");
 router.get("/all-products", async (req, res) => {
-  const shirtProducts = await ShirtProduct.find({});
-  const pantProducts = await PantProduct.find({});
-  const allProducts = [...shirtProducts, ...pantProducts];
-  res.json(allProducts).status(200);
+  try {
+    const shirtProducts = await ShirtProduct.find({});
+    const pantProducts = await PantProduct.find({});
+    const allProducts = [...shirtProducts, ...pantProducts];
+    res.json(allProducts).status(200);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
