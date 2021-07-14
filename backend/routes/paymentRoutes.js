e3da662380a0ac606e762762dc9540738eb2e9d1
Alfredo Mora <alfredomora@Alfredos-Mac-mini.local>
2021-07-13 20:20:19 -0700

    Found issue causing error

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const stripe = require('stripe')(process.env.SECRET_KEY);
+const stripe = require("stripe")(process.env.SECRET_KEY);
 
-const calculateOrderAmount = (cart) => {};
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { amount } = req.body;
   // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency: 'usd',
-  });
-  res.json({ client_secret: paymentIntent.client_secret });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+    });
+    res.json({ client_secret: paymentIntent.client_secret });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
