cb1c8df14ec7458cc192c7323c205acd4fb18eb8
Alfredo Mora <alfredomora@Alfredos-Mac-mini.local>
2021-07-14 17:50:07 -0700

    testing

--- backend/routes/pantProductsRoutes.js
+++ backend/routes/pantProductsRoutes.js
@@ -34,18 +34,30 @@ router.post("/pants", (req, res) => {
 
 //* PUT REQUEST
 //* PRIVATE
+// router.put("/pants/:id", async (req, res) => {
+//   const _id = req.params.id;
+//   const { category, product, image, sizes } = req.body;
+//   const updatedPant = {
+//     category,
+//     product,
+//     image,
+//     sizes,
+//   };
+//   category !== PANT
+//     ? res.json({ msg: "Incorrect category" }).status(400)
+//     : await PantProduct.replaceOne({ _id }, updatedPant);
+//   res.send("Updated").status(200);
+// });
+
 router.put("/pants/:id", async (req, res) => {
   const _id = req.params.id;
-  const { category, product, image, sizes } = req.body;
-  const updatedPant = {
-    category,
-    product,
-    image,
+  // const key = req.params.key;
+  // key !== process.env.KEY && res.json({ msg: "Not authorized" }).status(400);
+  const { sizes } = req.body;
+  const updatedPants = {
     sizes,
   };
-  category !== PANT
-    ? res.json({ msg: "Incorrect category" }).status(400)
-    : await PantProduct.replaceOne({ _id }, updatedPant);
+  await PantProduct.updateOne({ _id }, updatedPants);
   res.send("Updated").status(200);
 });
 module.exports = router;
