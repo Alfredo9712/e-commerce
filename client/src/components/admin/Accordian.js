607c574bcf494bed601987ada7f51db7a22cdfca
Alfredo Mora <alfredomora@Alfredos-Mac-mini.local>
2021-08-23 15:06:16 -0700

    more implementation

--- client/src/components/admin/Accordian.js
+++ client/src/components/admin/Accordian.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Accordion, Card, Button } from 'react-bootstrap';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import React from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
 
 const Accordian = () => {
   return (
-    <Accordion style={{ maxWidth: '800px' }}>
+    <Accordion style={{ maxWidth: "800px" }}>
       <Card>
         <Card.Header>
-          <Accordion.Toggle as={Button} variant='link' eventKey='0'>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
             <OpenInNewIcon />
           </Accordion.Toggle>
         </Card.Header>
-        <Accordion.Collapse eventKey='0'>
+        <Accordion.Collapse eventKey="0">
           <Card.Body>Hello! I'm the body</Card.Body>
         </Accordion.Collapse>
       </Card>
