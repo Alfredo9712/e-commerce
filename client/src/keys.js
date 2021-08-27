3ca2086d9ba633a45e840017e92a10f0317cad26
hector27 <hector27c7@gmail.com>
2021-08-27 14:38:01 -0700

    deployment

---
+++ client/src/keys.js
@@ -0,0 +1,10 @@
+export const keys = [
+  {
+    PUBLISHABLE_KEY:
+      'pk_test_51JCXOqDurmGdQKxEefA9iexcjp3cezr0SnTQd0BW69AOBHZZ1yoExAWTWv7Hrk2AJluHW4w6IPYcEvnFywMZMCai00hXCNeI4g',
+  },
+  {
+    SECRET_KEY:
+      'sk_test_51JCXOqDurmGdQKxE4PoiL9Pc0cphY2z0zQ5rkfRjvIBs4CgvBAFk8qL0kEZb0kLvkgrSwGHlaoOpw5lyFbHYKFB800mSsvsYQL',
+  },
+];
