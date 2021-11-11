f32b3fe5441a5adb6ad116a3a6a6b9dcd491fabb
Alfredo Mora <alfredomora@Alfredos-Mini.lan1>
2021-11-11 13:48:45 -0800

    Fixed redux devtools bug

--- backend/schema/orderSchema.js
+++ backend/schema/orderSchema.js
@@ -1,33 +1,31 @@
 const mongoose = require("mongoose");
-var date = new Date(); // Or the date you'd like converted.
-var isoDateTime = new Date(
-  date.getTime() - date.getTimezoneOffset() * 60000
-).toISOString();
 
-const orderSchema = mongoose.Schema({
-  amount: { type: Number, required: true },
-  complete: { type: Boolean, default: false },
-  order: [
-    {
-      price: { type: Number, required: true },
-      category: { type: String, required: true },
-      product: { type: String, required: true },
-      quantityPurchased: { type: Number, required: true },
-      sizePurchased: { type: String, required: true },
-    },
-  ],
-  billingDetails: {
-    email: { type: String, required: true },
-    name: { type: String, required: true },
-    address: {
-      city: { type: String, required: true },
-      line1: { type: String, requred: true },
-      state: { type: String, required: true },
-      postal_code: { type: String, required: true },
+const orderSchema = mongoose.Schema(
+  {
+    amount: { type: Number, required: true },
+    complete: { type: Boolean, default: false },
+    order: [
+      {
+        price: { type: Number, required: true },
+        category: { type: String, required: true },
+        product: { type: String, required: true },
+        quantityPurchased: { type: Number, required: true },
+        sizePurchased: { type: String, required: true },
+      },
+    ],
+    billingDetails: {
+      email: { type: String, required: true },
+      name: { type: String, required: true },
+      address: {
+        city: { type: String, required: true },
+        line1: { type: String, requred: true },
+        state: { type: String, required: true },
+        postal_code: { type: String, required: true },
+      },
     },
   },
-  createdAt: { type: Date, default: isoDateTime },
-});
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './bootstrap.min.css';
-import { Provider } from 'react-redux';
-import './App.css';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./bootstrap.min.css";
+import { Provider } from "react-redux";
+import "./App.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers";
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
